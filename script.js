5925c83044f93d979f953662929e5d516bac705f
Thien Nhan <thiennhanlop10b1@gmail.com>
2025-03-21 21:18:40 +0700

    Render workout input form

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map,mapEvent;
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -18,15 +18,36 @@ if (navigator.geolocation) {
       const longitude = position.coords.longitude;
 
       const coords=[latitude,longitude];
-      var map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click',function(mapEvent){
-        const {lat,lng}=mapEvent.latlng;
+      map.on('click',function(mapE){
+        mapEvent=mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+       
+      });
+
+      
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+} else {
+  alert('Geolocation is not supported by this browser.');
+}
+form.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+    // Clear input fields
+    inputDistance.value=inputCadence.value=inputDuration.value=inputElevation.value='';
+    //Display marker
+     const {lat,lng}=mapEvent.latlng;
         L.marker([lat,lng])
         .addTo(map)
         .bindPopup(L.popup({
@@ -39,14 +60,8 @@ if (navigator.geolocation) {
         .setPopupContent('Workout')
         .openPopup();
 
-      });
-
-      
-    },
-    function () {
-      alert('Could not get your position');
-    }
-  );
-} else {
-  alert('Geolocation is not supported by this browser.');
-}
+});
+inputType.addEventListener('change',function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
